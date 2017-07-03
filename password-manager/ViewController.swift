14d18bb78eb00a69150c3c84fead566515a7487e
sk-153353 <sk-153353@ap053130.hs-weingarten.de>
2017-07-03 12:47:36 +0200

    Initial Commit

---
+++ password-manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  password-manager
+//
+//  Created by sk-153353 on 03.07.17.
+//  Copyright © 2017 sk-ed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
